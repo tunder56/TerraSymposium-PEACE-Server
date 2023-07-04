31ea9700472b12a5c8b12d18cb6e19046b84390d
daniel m <65264071+tunder56@users.noreply.github.com>
2023-07-04 14:02:12 +1000

    Add project files.

---
+++ Hubs/Bunkershub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BunkersV3.Hubs
+{
+    public class Bunkershub : Hub
+    {
+
+
+
+        
+
+        public Task SendMessage(string user, string Message)
+        {
+            Console.WriteLine("incomming message from" + user);
+
+            return Clients.All.SendAsync(method: "reciveMessage", user, Message);
+        }
+
+        public Task AttackMessage(string Attacktype, List<string> Targets, string Attacker)
+        {
+            Console.WriteLine("attack method running");
+            return Clients.All.SendAsync(method: "incommingattack", Targets, Attacker, Attacktype);
+
+        }
+
+    }
+}
