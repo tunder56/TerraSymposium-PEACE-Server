fead440d8fef58e6699d636a200e0880c665b566
daniel m <65264071+tunder56@users.noreply.github.com>
2023-07-05 13:14:15 +1000

    Syncing the map

--- Hubs/Bunkershub.cs
+++ Hubs/Bunkershub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
 using System.Threading.Tasks;
 
 namespace TerraSymposium_PEACE_Server.Hubs
@@ -6,9 +7,26 @@ namespace TerraSymposium_PEACE_Server.Hubs
     public class Bunkershub : Hub
     {
 
+        [JsonObject]
+        public class city
+        {
+            [JsonProperty("name")] public string name;
+            [JsonProperty("x")] public int x;
+            [JsonProperty("y")] public int y;
+            [JsonProperty("owner")] public string owner;
+            [JsonProperty("ID")] public int ID;
+            [JsonProperty("Uid")] public int Uid;
+        }
+
+        public class citytosend
+        {
+            [JsonProperty("name")] public string name;
+            [JsonProperty("ID")] public int ID;
+            [JsonProperty("owner")] public string owner;
+        }
 
 
-        
+
 
         public async Task SendMessage(string user, string Message)
         {
@@ -22,6 +40,32 @@ namespace TerraSymposium_PEACE_Server.Hubs
             Console.WriteLine("attack method running");
             await Clients.All.SendAsync(method: "incommingattack", Targets, Attacker, Attacktype);
 
+        }
+
+
+        public Task Syncmap1(string cityin, string usersent)
+        {
+            Console.WriteLine($"map sync request from {usersent}");
+            ;
+            return Clients.Others.SendAsync(method: "Syncmap1", cityin);
+        }
+        public Task Syncmap2(string cityin, string usersent)
+        {
+            Console.WriteLine($"map sync request from {usersent}");
+
+            return Clients.Others.SendAsync(method: "Syncmap2", cityin);
+        }
+        public Task Syncmap3(string cityin, string usersent)
+        {
+            Console.WriteLine($"map sync request from {usersent}");
+
+            return Clients.Others.SendAsync(method: "Syncmap3", cityin);
+        }
+        public Task Syncmap4(string cityin, string usersent)
+        {
+            Console.WriteLine($"map sync request from {usersent}");
+
+            return Clients.Others.SendAsync(method: "Syncmap4", cityin);
         }
 
     }
